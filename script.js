4ed1628c47456985f74207ed756f104321dcb42c
avi9360 <savinash406@gmail.com>
2021-12-08 13:40:26 +0530

    Final result

--- script.js
+++ script.js
@@ -37,6 +37,7 @@ function loadLabeledImages() {
     labels.map(async label => {
       const descriptions = [];
       for (let i = 1; i <= 2; i++) {
+        https://github.com/ImAvinashSharma/abhi-chu/tree/main/labeled_images/Abhi
         const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`);
         const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor();
         descriptions.push(detections.descriptor);
